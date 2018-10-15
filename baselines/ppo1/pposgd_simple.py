60f6fac9d6b60441f4b2eded0afcd1cfc9a20659
luckeciano <luckeciano@gmail.com>
2018-10-15 00:40:17 -0300

    Some adjustments for kick policy: bug correction for initialization, better hypers and stochastic version

--- baselines/ppo1/pposgd_simple.py
+++ baselines/ppo1/pposgd_simple.py
@@ -134,6 +134,7 @@ def learn(env, policy_fn, *,
         for (oldv, newv) in zipsame(oldpi.get_variables(), pi.get_variables())])
     compute_losses = U.function([ob, ac, atarg, ret, lrmult], losses)
 
+    initialize_uninitialized(tf.get_default_session())
     #U.initialize()
     adam.sync()
 
@@ -240,3 +241,13 @@ def learn(env, policy_fn, *,
 
 def flatten_lists(listoflists):
     return [el for list_ in listoflists for el in list_]
+
+def initialize_uninitialized(sess):
+    global_vars          = tf.global_variables()
+    is_not_initialized   = sess.run([tf.is_variable_initialized(var) for var in global_vars])
+    not_initialized_vars = [v for (v, f) in zip(global_vars, is_not_initialized) if not f]
+
+    for i in not_initialized_vars:
+        print (str(i.name))
+    if len(not_initialized_vars):
+        sess.run(tf.variables_initializer(not_initialized_vars))
--- baselines/ppo1/run_soccer.py
+++ baselines/ppo1/run_soccer.py
@@ -27,11 +27,11 @@ def train(env_id, num_timesteps, seed, save_model, load_model, model_dir):
     env = bench.Monitor(env, logger.get_dir())
     env.seed(workerseed)
     gym.logger.setLevel(logging.WARN)
-    pposgd_simple.learn(env, policy_fn,
+    pposgd_simple.learn(env, policy_fn, 
                         max_timesteps=num_timesteps,
-                        timesteps_per_actorbatch=256,
+                        timesteps_per_actorbatch=4096,
                         clip_param=0.2, entcoeff=0.0,
-                        optim_epochs=10, optim_stepsize=1e-3, optim_batchsize=128,
+                        optim_epochs=10, optim_stepsize=1e-6, optim_batchsize=2048,
                         gamma=0.99, lam=0.95, schedule='linear',
                         save_model=save_model, load_model=load_model, model_dir=model_dir
                         )
