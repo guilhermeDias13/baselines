4feff9f1079ce90faebc964b8d2f298711c6ae3b
luckeciano <luckeciano@gmail.com>
2019-03-15 20:09:19 -0300

    Adding hyper search in walk policy

--- baselines/ppo1/walk_policy.py
+++ baselines/ppo1/walk_policy.py
@@ -17,7 +17,7 @@ class WalkPolicy(object):
             self._init(*args, **kwargs)
             self.scope = tf.get_variable_scope().name
             
-    def _init(self, ob_space, ac_space, hid_size, num_hid_layers, gaussian_fixed_var=True):
+    def _init(self, ob_space, ac_space, hid_size, num_hid_layers, exploration_rate, gaussian_fixed_var=True):
         assert isinstance(ob_space, gym.spaces.Box)
 
         self.pdtype = pdtype = make_pdtype(ac_space)
@@ -45,7 +45,7 @@ class WalkPolicy(object):
         
         if gaussian_fixed_var and isinstance(ac_space, gym.spaces.Box):
             mean = model.output            
-            logstd = tf.get_variable(name="logstd", shape=[1, pdtype.param_shape()[0]//2], initializer=tf.constant_initializer(-3))
+            logstd = tf.get_variable(name="logstd", shape=[1, pdtype.param_shape()[0]//2], initializer=tf.constant_initializer(exploration_rate))
             pdparam = tf.concat([mean, mean * 0.0 + logstd], axis=1)
         else:
             pdparam = tf.layers.dense(model.output, pdtype.param_shape()[0], "polfinal", U.normc_initializer(0.01))
